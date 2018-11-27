687f149625d3402a82757eb54cc03dd46544fb99
Antoine Voiry <antoine.voiry@gmail.com>
2018-11-27 23:18:05 +0000

    backup

--- .vscode.back/python.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set v_params=%*
-set v_params=%v_params:\=/%
-set v_params=%v_params:c:=/mnt/c%
-set v_params=%v_params:"=\"%
-bash.exe -c "/home/avoiry/.local/share/virtualenvs/temp-and-humidity-cysFBGEl/bin/python %v_params%"
